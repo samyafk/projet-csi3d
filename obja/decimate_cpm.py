f6aafb8607147b3eb097203310b0d257df090710
Thomas Bocandé <thomasbocande@gmail.com>
2024-10-11 08:52:01 +0200

    ajout liste des edges

--- obja/decimate_cpm.py
+++ obja/decimate_cpm.py
@@ -13,43 +13,49 @@ class Decimater(obja.Model):
         self.deleted_faces = set()
 
     def contract(self, output):
-        """
-        Decimates the model stupidly, and write the resulting obja in output.
-        """
+        
         operations = []
+        edges = []
 
+        for (face_index, face) in enumerate(self.faces):
+            edges.append((face.a,face.b))
+            edges.append((face.b,face.c))
+            edges.append((face.a,face.c))
+
+        collapse = [True for i in range(len(edges))]
+        
+        print(edges)
+        #print(collapse)
+
+        # # Iterate through the vertex
         # for (vertex_index, vertex) in enumerate(self.vertices):
-        #     operations.append(('ev', vertex_index, vertex + 0.25))
 
-        # Iterate through the vertex
-        for (vertex_index, vertex) in enumerate(self.vertices):
+        #     # Iterate through the faces
+        #     for (face_index, face) in enumerate(self.faces):
 
-            # Iterate through the faces
-            for (face_index, face) in enumerate(self.faces):
+        #         # Delete any face related to this vertex
+        #         if face_index not in self.deleted_faces:
+        #             if vertex_index in [face.a,face.b,face.c]:
+        #                 self.deleted_faces.add(face_index)
+        #                 # Add the instruction to operations stack
+        #                 operations.append(('face', face_index, face))
 
-                # Delete any face related to this vertex
-                if face_index not in self.deleted_faces:
-                    if vertex_index in [face.a,face.b,face.c]:
-                        self.deleted_faces.add(face_index)
-                        # Add the instruction to operations stack
-                        operations.append(('face', face_index, face))
+        #     # Delete the vertex
+        #     operations.append(('vertex', vertex_index, vertex))
 
-            # Delete the vertex
-            operations.append(('vertex', vertex_index, vertex))
+        # # To rebuild the model, run operations in reverse order
+        # operations.reverse()
 
-        # To rebuild the model, run operations in reverse order
-        operations.reverse()
+        # # Write the result in output file
+        # output_model = obja.Output(output, random_color=True)
 
-        # Write the result in output file
-        output_model = obja.Output(output, random_color=True)
-
-        for (ty, index, value) in operations:
-            if ty == "vertex":
-                output_model.add_vertex(index, value)
-            elif ty == "face":
-                output_model.add_face(index, value)   
-            else:
-                output_model.edit_vertex(index, value)
+        # for (ty, index, value) in operations:
+        #     if ty == "vertex":
+        #         output_model.add_vertex(index, value)
+        #     elif ty == "face":
+        #         output_model.add_face(index, value)   
+        #     else:
+        #         output_model.edit_vertex(index, value)
 
 def main():
     """
